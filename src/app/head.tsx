75bb6c73c422a6ee49c898530ca99055600d9d92
TakayukiTooyama <t.tooyama107@gmail.com>
2023-01-26 01:26:21 +0900

    :shirt: ESLint修正

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,9 +2,12 @@ const Head = () => {
   return (
     <>
       <title>Next.js sterter template</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Next.js13xMantineのテンプレートです。" />
-      <link rel="icon" href="/favicon.ico" />
+      <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <meta
+        name='description'
+        content='Next.js13xMantineのテンプレートです。'
+      />
+      <link rel='icon' href='/favicon.ico' />
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/line-clamp')],
-}
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+  ],
+};
